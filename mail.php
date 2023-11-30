68ec5b3d7a1bca37c32ac67ef1092a7e5a782828
Marcus Vinicius <marcuspaixao0@gmail.com>
2023-11-30 12:46:31 -0300

    Atualização Hospedagem

--- mail.php
+++ mail.php
@@ -13,23 +13,23 @@
         if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Preencha o formulário e tente novamente.";
             exit;
         }
 
         // Set the recipient email address.
         // Note:  Update this to your desired email address.
-        $recipient = "modinatheme@gmail.com";
+        $recipient = "marcuspaixao0@gmail.com";
 
         // Set the email subject.
-        $subjectname = "New Contact $subject";
+        $subjectname = "Nono Contato $subject";
 
         // Build the email content.
-        $email_content = "Name: $name  \r\n\n";
-        $email_content .= "Email: $email \r\n\n";
-        $email_content .= "Subject: $subject \r\n\n";
-        $email_content .= "Phone: $phone \r\n\n";
-        $email_content .= "Message: $message \r\n\n";
+        $email_content = "Nome: $name  \r\n\n";
+        $email_content .= "E-mail: $email \r\n\n";
+        $email_content .= "Assunto: $subject \r\n\n";
+        $email_content .= "Telefone: $phone \r\n\n";
+        $email_content .= "Mensagem: $message \r\n\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
@@ -38,17 +38,17 @@
         if (mail($recipient, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Obrigado! Sua mensagem foi enviada.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Ops! Algo deu errado e não foi possível enviar sua mensagem.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Houve um problema com seu envio. Tente novamente.";
     }
 
 ?>
